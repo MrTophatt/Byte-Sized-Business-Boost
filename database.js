0987911be8f5062111e696e1b33138ff382ceeb0
Mohid Faisal Mushtaq <mohid.mushtaq1@gmail.com>
2026-01-28 14:16:31 -0500

    Removed src folder and reverted to previous file and folder structure before initial commit

---
+++ database.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+async function connectDB() {
+    if (mongoose.connection.readyState === 1) return;
+
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+}
+
+async function disconnectDB() {
+    if (mongoose.connection.readyState !== 0) {
+        await mongoose.disconnect();
+        console.log("MongoDB disconnected");
+    }
+}
+
+module.exports = { connectDB, disconnectDB };
